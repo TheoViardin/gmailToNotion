3aac96a4fb22bff4b098ff6ab9311a1257954da6
TheoViardin <theoviardin@gmail.com>
2024-09-30 11:37:16 +0200

    fix: cut attachment to max length in notion

--- services/notion.js
+++ services/notion.js
@@ -276,7 +276,7 @@ async function addClientMails(userConfig, existingClients, client, emails) {
                 external: {
                   url: file.url,
                 },
-                name: file.name,
+                name: file.name.substring(0, 99),
               },
             })),
           ],
